ad3ccacdef07c546461e992dffa3fb7a4a7a3df6
Ashish Madan <ashish@veersatech.com>
2024-02-17 05:41:57 +0530

    Fixed satus mapping before checking Authentication

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,11 +6,10 @@ import jwt from "jsonwebtoken";
 export const isAuthenticated = async (req, res, next) => {
   const { token } = req.cookies;
   if (!token) {
-    res.status(200).json({
+    return res.status(404).json({
       success: false,
       msg: "Login First !!",
     });
-    return;
   }
   const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
   const user = await User.findById(decodedToken._id);
